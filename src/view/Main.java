eeeed7d10ba4a361aa53c771aba3f17a35f8039c
luiz <luiz@pop-os.localdomain>
2023-05-12 02:21:50 -0300

    Commit final

---
+++ src/view/Main.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int nat=1234;
+		int soma = recursaoNat(nat);
+		System.out.println(soma);
+		
+	}
+
+	private static int recursaoNat(int nat) {
+		if(nat<=0) { // condição de parada
+			return 0;
+		}else {
+			return nat + recursaoNat(nat-1); // chamada de recursão
+		}
+	}
+}
